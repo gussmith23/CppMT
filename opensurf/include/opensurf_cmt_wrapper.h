47dbfbe4efee107bbc97f60fa57c58d23924291a
Gus Smith <gus@justg.us>
2016-02-26 13:15:21 -0500

    begins building wrapper for OpenSURF

---
+++ opensurf/include/opensurf_cmt_wrapper.h
@@ -0,0 +1,21 @@
+/***
+ * SURFWrapper: a wrapper for OpenSURF to make it "look like" an OpenCV 
+ * detector/descriptor.
+ * Author: Gus Smith <hfs5022@psu.edu> or <gus@justg.us>
+ */
+
+#ifndef OPENSURF_CMT_WRAPPER_H
+#define OPENSURF_CMT_WRAPPER_H
+
+#include "surflib.h"
+#include <opencv.hpp>
+
+class SURFWrapper{
+public:
+	//SURFWrapper SURFWrapper::create(const string& descriptorExtractorType);
+	void SURFWrapper::detect(const Mat& image, vector<KeyPoint>& keypoints, const Mat& mask = Mat());
+	void SURFWrapper::compute(const Mat& image, vector<KeyPoint>& keypoints, Mat& descriptors);
+};
+
+
+#endif 
---
+++ opensurf/src/opensurf_cmt_wrapper.cpp
@@ -0,0 +1,12 @@
+#include "opensurf_cmt_wrapper.h"
+
+void SURFWrapper::detect(const Mat& image, vector<KeyPoint>& keypoints, const Mat& mask = Mat())
+{
+
+}
+
+
+void SURFWrapper::compute(const Mat& image, vector<KeyPoint>& keypoints, Mat& descriptors)
+{
+
+}
