8b512317d75fe49c4c23b6e606c7920beb357a57
Gus Smith <gus@justg.us>
2016-03-01 17:02:23 -0500

    adds comments to wrapper source

--- opensurf/src/opensurf_cmt_wrapper.cpp
+++ opensurf/src/opensurf_cmt_wrapper.cpp
@@ -1,11 +1,26 @@
 #include "opensurf_cmt_wrapper.h"
 
+
+/*
+Detect wraps the following OpenSURF (surflib.h) function call:
+inline void surfDet(IplImage *img,  image to find Ipoints in 
+                    std::vector<Ipoint> &ipts,  reference to vector of Ipoints 
+                    int octaves = OCTAVES, number of octaves to calculate 
+                    int intervals = INTERVALS,  number of intervals per octave 
+                    int init_sample = INIT_SAMPLE,  initial sampling step 
+                    float thres = THRES  blob response threshold 
+*/
 void SURFWrapper::detect(const Mat& image, vector<KeyPoint>& keypoints, const Mat& mask = Mat())
 {
 
 }
 
-
+/*
+Compute wraps the following OpenSURF (surflib.h) function call:
+inline void surfDes(IplImage *img,   image to find Ipoints in 
+                    std::vector<Ipoint> &ipts,  reference to vector of Ipoints 
+                    bool upright = false)  run in rotation invariant mode? 
+*/
 void SURFWrapper::compute(const Mat& image, vector<KeyPoint>& keypoints, Mat& descriptors)
 {
 
