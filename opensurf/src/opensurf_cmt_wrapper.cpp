bf9fa93f02da73dc28a36da732e0133923d2a8cd
Gus Smith <gus@justg.us>
2016-03-01 18:40:06 -0500

    adds preliminary version of detect

--- opensurf/src/opensurf_cmt_wrapper.cpp
+++ opensurf/src/opensurf_cmt_wrapper.cpp
@@ -13,7 +13,25 @@ inline void surfDet(IplImage *img,  image to find Ipoints in
 */
 void SURFWrapper::detect(const Mat& image, vector<KeyPoint>& keypoints, const Mat& mask = Mat())
 {
+	IplImage *iplImage = new IplImage(image);
+	vector<Ipoint> keypoints_ipoints = vector<Ipoint>();
 
+	surfDet(iplImage, keypoints_ipoints);
+
+	// Now we convert each ipoint in keypoint_ipoints to KeyPoint.
+	for each (Ipoint ipoint in keypoints_ipoints)
+	{
+		KeyPoint keypoint = KeyPoint();
+
+		// TODO we need to figure out which of the KeyPoint fields are actually used,
+		//	and make sure those are filled out here.
+		keypoint.pt = Point2f(ipoint.x, ipoint.y);
+		keypoint.angle = ipoint.orientation;		// TODO may have to shift this here.
+		keypoint.octave;
+		keypoint.size = ipoint.scale;				// TODO don't think this is right.
+
+		keypoints.push_back(keypoint);
+	}
 }
 
 /*
